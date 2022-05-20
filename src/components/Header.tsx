a66eda3b979c52a89a8b9ea343230c1d72e892f7
yoon_z <yoon_z@iyunjiui-MacBook-Pro.local>
2022-05-20 15:07:05 +0900

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <h1>My Wish List</h1>
+    <button>위시리스트</button>
+    <button>구매한 상품</button>
+    </>
+  )
+}
+
+export default Header;
